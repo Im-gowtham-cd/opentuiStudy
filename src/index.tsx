4706e606015acc05dde39453807023b6f8edee0a
Im-gowtham-cd <gowthamcdstudies@gmail.com>
2026-06-02 18:26:29 +0530

    Allign --Fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,8 @@ import { Navbar } from './components/index'
 
 function App() {
   return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
+    <box alignItems="center" justifyContent="flex-start" flexGrow={1}>
       <Navbar />
-      <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="block" text="GoTM" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
-      </box>
     </box>
   );
 }
